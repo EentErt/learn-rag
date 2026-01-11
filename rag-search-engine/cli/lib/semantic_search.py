467175a8486065a2d0310b49351b6f3e5d0f7dbc
EentErt <nate.giff@gmail.com>
2026-01-11 12:28:46 -0800

    adding semantic search with chunks

--- rag-search-engine/cli/lib/semantic_search.py
+++ rag-search-engine/cli/lib/semantic_search.py
@@ -1,12 +1,140 @@
 from sentence_transformers import SentenceTransformer
-
+import numpy as np
+import json
+import os
+import re
 
 class SemanticSearch():
     def __init__(self):
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
+
+    def search(self, query, limit):
+        if self.embeddings is None:
+            raise ValueError("No embeddings loaded. Call `load_or_create_embeddings` first.")
+        query = self.generate_embedding(query)
+        similarities = []
+        for i in range(len(self.embeddings)):
+            similarity = cosine_similarity(query, self.embeddings[i])
+            similarities.append((similarity, self.documents[i]))
+
+        similarities.sort(key=lambda x: x[0], reverse=True)
+        result = []
+        for i in range(limit):
+            result.append({
+                "score": similarities[i][0],
+                "title": similarities[i][1]["title"],
+                "description": similarities[i][1]["description"]
+            })
+        return result
+
+    def generate_embedding(self, text):
+        if text.strip() == "":
+            raise ValueError("Input text cannot be empty.")
+
+        embedding = self.model.encode([text])
+
+        return embedding[0]
+
+    def load_or_create_embeddings(self, documents):
+        try:
+            with open("cache/movie_embeddings.npy", "rb") as file:
+                self.embeddings = np.load(file)
+            if len(self.embeddings) == len(self.documents):
+                return self.embeddings
+        except:
+            return self.build_embeddings(documents)
+
+    def build_embeddings(self, documents):
+        self.documents = documents
+        doc_list = []
+        for doc in documents:
+            self.document_map[doc['id']] = doc
+            doc_list.append( f"{doc['title']}: {doc['description']}")
+        self.embeddings = self.model.encode(doc_list, show_progress_bar=True)
+        if not os.path.exists("cache"):
+            os.makedirs("cache")
+        with open("cache/movie_embeddings.npy", "wb") as file:
+            np.save(file, self.embeddings)
+        return self.embeddings
+
+class ChunkedSemanticSearch(SemanticSearch):
+    def __init__(self, model_name="all-MiniLM-L6-v2"):
+        super().__init__(model_name)
+        self.chunk_embeddings = None
+        self.chunk_metadata = None
+
+def cosine_similarity(vec1, vec2):
+    dot_product = np.dot(vec1, vec2)
+    norm1 = np.linalg.norm(vec1)
+    norm2 = np.linalg.norm(vec2)
+
+    if norm1 == 0 or norm2 == 0:
+        return 0.0
+
+    return dot_product / (norm1 * norm2)
+
+def embed_query_text(query):
+    sem_search = SemanticSearch()
+    embedding = sem_search.generate_embedding(query)
+    print(f"Query: {query}")
+    print(f"First 5 dimensions: {embedding[:5]}")
+    print(f"Shape: {embedding.shape}")
+
+def verify_embeddings():
+    sem_search = SemanticSearch()
+    with open("data/movies.json") as file:
+        movies = json.load(file)["movies"]
+    embeddings = sem_search.load_or_create_embeddings(movies)
+    print(f"Number of docs: {len(movies)}")
+    print(f"Embeddings shape: {embeddings.shape[0]} vectors in {embeddings.shape[1]} dimensions")
+
+def embed_text(text):
+    sem_search = SemanticSearch()
+    embedding = sem_search.generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
 
 def verify_model():
     semantic_search = SemanticSearch()
     print(f"Model loaded: {semantic_search.model}")
     print(f"Max sequence length: {semantic_search.model.max_seq_length}")
+
+def chunk_text(text, chunk_size, overlap):
+    words = text.split()
+    chunks = []
+    over = ""
+    while len(words) > chunk_size:
+        chunk = ""
+        if over:
+            chunk = over + " "
+        chunk += " ".join(words[:chunk_size])
+        chunks.append(chunk)
+        if overlap > 0:
+            over = " ".join(words[chunk_size - overlap:chunk_size])
+        words = words[chunk_size:]
+    if over:
+        chunks.append(over + " " +" ".join(words))
+    else:
+        chunks.append(" ".join(words))
+    return chunks
+
+def semantic_chunk_text(text, max, overlap):
+    sentences = re.split(r"(?<=[.!?])\s+", text)
+    chunks = []
+    over = ""
+    while len(sentences) > max:
+        chunk = " ".join(sentences[:max])
+        chunks.append(chunk)
+        if overlap > 0:
+            over = " ".join(sentences[max - overlap:max])
+        sentences = sentences[max:]
+    if over:
+        chunks.append(over + " " + " ".join(sentences))
+    else:
+        chunks.append(" ".join(sentences))
+    return chunks
 
--- rag-search-engine/cli/semantic_search_cli.py
+++ rag-search-engine/cli/semantic_search_cli.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.semantic_search import SemanticSearch, verify_model
+from lib.semantic_search import verify_model, embed_text, verify_embeddings, embed_query_text, SemanticSearch, chunk_text, semantic_chunk_text
+import json
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -9,11 +10,59 @@ def main():
 
     verify_parser = subparsers.add_parser("verify", help="verify semantic search setup")
 
+    verify_embeddings_parser = subparsers.add_parser("verify_embeddings", help="Verify embeddings")
+
+    embed_query_parser = subparsers.add_parser("embedquery", help="Embed a given query text")
+    embed_query_parser.add_argument("query", type=str, help="Query text to embed")
+
+    embed_text_parser = subparsers.add_parser("embed_text", help="Embed a given text")
+    embed_text_parser.add_argument("text", type=str, help="Text to embed")
+
+    search_parser = subparsers.add_parser("search", help="Perform semantic search")
+    search_parser.add_argument("query", type=str, help="Query text to search for")
+    search_parser.add_argument("--limit", type=int, nargs="?", default=5, help="Number of results to return")
+
+    chunk_parser = subparsers.add_parser("chunk", help="Chunk a given text")
+    chunk_parser.add_argument("text", type=str, help="Text to chunk")
+    chunk_parser.add_argument("--chunk-size", type=int, nargs="?", default=200, help="Size of each chunk")
+    chunk_parser.add_argument("--overlap", type=int, nargs="?", default=0, help="Overlap between chunks")
+
+    semantic_chunk_parser = subparsers.add_parser("semantic_chunk", help="Chunk text semantically")
+    semantic_chunk_parser.add_argument("text", type=str, help="Text to chunk")
+    semantic_chunk_parser.add_argument("--max-chunk-size", type=int, nargs="?", default=4, help="Maximum chunk size")
+    semantic_chunk_parser.add_argument("--overlap", type=int, nargs="?", default=0, help="Overlap between chunks")
+
+
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+        case "embed_text":
+            embed_text(args.text)
+        case "verify_embeddings":
+            verify_embeddings()
+        case "embedquery":
+            embed_query_text(args.query)
+        case "search":
+            sem_search = SemanticSearch()
+            with open("data/movies.json") as file:
+                movies = json.load(file)["movies"]
+            embeddings = sem_search.load_or_create_embeddings(movies)
+            results = sem_search.search(args.query, args.limit)
+            for i in range(len(results)):
+                print(f"{i+1}. {results[i]["title"]} (score: {results[i]["score"]:.4f})")
+                print(f"   {results[i]["description"]}")
+        case "chunk":
+            chunks = chunk_text(args.text, args.chunk_size, args.overlap)
+            print(f"Chunking {len(args.text)} characters")
+            for i in range(len(chunks)):
+                print(f"{i+1}. {chunks[i]}")
+        case "semantic_chunk":
+            chunks = semantic_chunk_text(args.text, args.max_chunk_size, args.overlap)
+            print(f"Semantically chunking {len(args.text)} characters")
+            for i in range(len(chunks)):
+                print(f"{i+1}. {chunks[i]}")
         case _:
             parser.print_help()
 
